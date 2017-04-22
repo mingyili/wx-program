e0b9bb9d53052fff7781fc3b2d66df3138278aa6
mingyili <1275656592@qq.com>
2017-04-22 18:34:33 +0800

    first time push

---
+++ pages/alert/alert.js
@@ -0,0 +1,13 @@
+//alert.js
+//获取应用实例
+var app = getApp();
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    //alert('onload');
+  }
+})
